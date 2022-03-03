3af1c583f321fced384fb4ab08380d5c4379a813
Sheva Athalla <shevaathalla@gmail.com>
2022-03-03 15:29:10 +0700

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,107 @@
+import { LinkIcon, MailIcon, PhoneIcon } from "@heroicons/react/solid";
+import React from "react";
+
+export default function Contact() {
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+      )
+      .join("&");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", name, email, message }),
+    })
+      .then(() => alert("Message sent!"))
+      .catch((error) => alert(error));
+  }
+
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            title="map"
+            className="absolute inset-0"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            style={{ filter: "opacity(0.7)" }}
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126438.33806413783!2d112.56174177578488!3d-7.978469457276236!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd62822063dc2fb%3A0x78879446481a4da2!2sMalang%2C%20Malang%20City%2C%20East%20Java!5e0!3m2!1sen!2sid!4v1646194825887!5m2!1sen!2sid"
+          />
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                Malang <br />
+                East Java, Indonesia
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a
+                href="mailto:shevaathalla@gmail.com"
+                className="text-indigo-400 leading-relaxed"
+              >
+                shevaathalla@gmail.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">+6282330907914</p>
+            </div>
+          </div>
+        </div>
+        <form
+          netlify
+          name="contact"
+          className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+        >
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            Hire Me
+          </h2>
+          <p className="leading-relaxed mb-5">
+            Well i currently have a lot of free time, so I am open to any
+            freelance project as a frontend or backend web developer
+          </p>
+          <a
+            href="https://wa.me/6282330907914"
+            className="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg mb-4"
+          >
+            <PhoneIcon className="w-10 h-10 mr-4 inline-block"/>         
+            Whatsapp
+          </a>
+          <a
+            href="mailto:shevaathalla@gmail.com"
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg mb-4"
+          >
+          <MailIcon className="w-10 h-10 mr-4 inline-block" />
+            Mail
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sheva-athalla/"
+            className="text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg"
+          >
+          <LinkIcon className="w-10 h-10 mr-4 inline-block" />
+            LinkedIn
+          </a>
+        </form>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        title: "Stuck Neverflow",
+        subtitle: "Stack Overflow Cheap Clone",
+        description: "forum application that makes it easy for users to find an answer",
+        image: "./stuck_neverflow.gif",
+        link: "https://github.com/shevaathalla/stuck-neverflow",
+    },
+    {
+        title: "Campus Facility Informatics System",
+        subtitle: "Loaning Tool System in Maulana Malik Ibrahim Malang",
+        description: "with this app borrowing tools will be easier to monitor",
+        image: "./sisfo_fasilitas.gif",
+        link: "https://github.com/shevaathalla/sisfo-fasilitas",
+    },
+    {
+        title: "Larastock",
+        subtitle: "React Typescript Stock Management System",
+        description: "a stock Management system application that build using react and laravel",
+        image: "./larastock.gif",
+        link: "https://github.com/shevaathalla/reactts-larastock",
+    },
+    {
+        title: "Web Profile",
+        subtitle: "My Personal Web Portfolio",
+        description: "I use this website to introduce myself to the public",
+        image: "./portfolio.gif",
+        link: "https://github.com/shevaathalla/reactts-larastock",
+    }
+];
+
+export const skills = [
+    "Laravel",
+    "Node JS",
+    "React JS",
+    "Wordpress",
+    "Automation Test using PEST PHP"
+];
