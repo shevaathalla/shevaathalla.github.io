0cca3b5012e9df2c0f51b68fc64092fb69067945
Sheva Athalla <shevaathalla@gmail.com>
2022-07-02 16:10:53 +0700

    add new project and update skills

--- src/data.js
+++ src/data.js
@@ -3,37 +3,44 @@ export const projects = [
         title: "Stuck Neverflow",
         subtitle: "Stack Overflow Cheap Clone",
         description: "forum application that makes it easy for users to find an answer",
-        image: "./stuck_neverflow.gif",
+        image: "./projects/stuck_neverflow.gif",
         link: "https://github.com/shevaathalla/stuck-neverflow",
     },
     {
         title: "Campus Facility Informatics System",
         subtitle: "Loaning Tool System in Maulana Malik Ibrahim Malang",
         description: "with this app borrowing tools will be easier to monitor",
-        image: "./sisfo_fasilitas.gif",
+        image: "./projects/sisfo_fasilitas.gif",
         link: "https://github.com/shevaathalla/sisfo-fasilitas",
     },
     {
         title: "Larastock",
         subtitle: "React Typescript Stock Management System",
         description: "a stock Management system application that build using react and laravel",
-        image: "./larastock.gif",
+        image: "./projects/larastock.gif",
         link: "https://github.com/shevaathalla/reactts-larastock",
     },
     {
         title: "Web Profile",
         subtitle: "My Personal Web Portfolio",
         description: "I use this website to introduce myself to the public",
-        image: "./portfolio.gif",
+        image: "./projects/portfolio.gif",
         link: "https://github.com/shevaathalla/shevaathalla.github.io",
+    },
+    {
+        title: "Nickel Drive",
+        subtitle: "Rental Application with multiple user level authentication",
+        description: "I use this website to complete my assessment in the Sekawan Media",
+        image: "./projects/nickel_drive.gif",
+        link: "https://github.com/shevaathalla/sekawan-media-assessment",
     }
 ];
 
 export const skills = [
-    "Java Programming Language",
+    "Java",
+    "PHP",
     "Laravel",
-    "Node JS",
+    "Express JS",
     "Nest JS",
-    "React JS",    
-    "Automation Test using PEST PHP"
+    "React JS",
 ];
